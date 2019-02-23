bbf67916894bcc6ec87b36b759b62ddb7e123cca
Leah Miller <lkmiller.87@gmail.com>
2019-02-23 11:22:21 -0700

    Adds test setup for location api

---
+++ spec/requests/api/v1/location_request_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'Location API' do
+  it "sends the city and state" do
+    location_data = create(:location)
+
+  get "/api/v1/forecast?location=#{location_data}"
+
+  location = JSON.parse(response.body)
+
+  expect(repsonse).to be_successful
+  expect(location["city"]).to eq(location["city"])
+  expect(location["state"]).to eq(location["state"])
+  end
+end
