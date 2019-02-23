2dffefe0544b74167a41b43644e273ff4fece8a3
Leah Miller <lkmiller.87@gmail.com>
2019-02-23 13:03:55 -0700

    Adds faraday block and other json parsing methods to geocoding service

--- app/services/geocoding_service.rb
+++ app/services/geocoding_service.rb
@@ -2,4 +2,28 @@ class GeocodingService
   def initialize(location)
     @location = location
   end
+
+  def find_latitude
+    get_coordinates[:results].first[:geometry][:location][:lat]
+  end
+
+  def find_longitude
+    get_coordinates[:results].first[:geometry][:location][:lng]
+  end
+
+  private
+
+  def get_coordinates
+    response = conn.get("/maps/api/geocode/json") do |f|
+      f.params[:address] = "#{@location.city},#{@location.state}"
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new('https://maps.googleapis.com') do |faraday|
+      faraday.params[:key] = ENV['google_maps_api_key']
+      faraday.adapter Faraday.default_adapter
+    end
+  end
 end
