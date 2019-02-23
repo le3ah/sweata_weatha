06f9fcb30680c455e0c8e32eeb744828c4d6492d
Leah Miller <lkmiller.87@gmail.com>
2019-02-23 11:59:03 -0700

    Adds validation test for location

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+  end
 end
