19085761e980dc781bafaead2aa7fc3e5a754cdc
Leah Miller <lkmiller.87@gmail.com>
2019-02-23 13:06:03 -0700

    Adds refactor of get_json method to pass in url string

--- app/services/geocoding_service.rb
+++ app/services/geocoding_service.rb
@@ -4,17 +4,19 @@ class GeocodingService
   end
 
   def find_latitude
-    get_coordinates[:results].first[:geometry][:location][:lat]
+    get_json("/maps/api/geocode/json")[:results]
+    .first[:geometry][:location][:lat]
   end
 
   def find_longitude
-    get_coordinates[:results].first[:geometry][:location][:lng]
+    get_json("/maps/api/geocode/json")[:results]
+    .first[:geometry][:location][:lng]
   end
 
   private
 
-  def get_coordinates
-    response = conn.get("/maps/api/geocode/json") do |f|
+  def get_json(url)
+    response = conn.get(url) do |f|
       f.params[:address] = "#{@location.city},#{@location.state}"
     end
     JSON.parse(response.body, symbolize_names: true)
