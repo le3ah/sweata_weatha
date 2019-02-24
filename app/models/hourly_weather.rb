296b340a8320c98d497e8e0349b67fff8abd6604
Leah Miller <lkmiller.87@gmail.com>
2019-02-24 15:26:38 -0700

    Removes tonight summary from hourly weather attributes

--- app/models/hourly_weather.rb
+++ app/models/hourly_weather.rb
@@ -1,6 +1,5 @@
 class HourlyWeather
   def initialize(attributes)
-    # @tonight_summary = attributes.last[:summary]
     @hourly_time = Time.at(attributes[:time]).strftime("%l")
     @hourly_temperature = attributes[:temperature]
     @hourly_icon = attributes[:icon]
