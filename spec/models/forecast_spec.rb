c259e343e3dbf16dd2b26019b803a4ba9c25197f
Leah Miller <lkmiller.87@gmail.com>
2019-02-24 15:25:32 -0700

    Adds model test for get tonight summary

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -6,14 +6,20 @@ describe Forecast do
     forecast = Forecast.new(location)
     expect(forecast).to be_a(Forecast)
   end
-  it ".get_latitude", :vcr do
+  it "#get_latitude", :vcr do
     location = "Denver,CO"
     forecast = Forecast.new(location)
     expect(forecast.get_latitude).to eq(39.7392358)
   end
-  it ".get_longitude", :vcr do
+  it "#get_longitude", :vcr do
     location = "Denver,CO"
     forecast = Forecast.new(location)
     expect(forecast.get_longitude).to eq(-104.990251)
+  end
+  it "#get_tonight_summary", :vcr do
+    location = "Denver,CO"
+    forecast = Forecast.new(location)
+    summary = forecast.weather_data[:hourly][:data].last[:summary]
+    expect(forecast.get_tonight_summary).to eq(summary)
   end
 end
