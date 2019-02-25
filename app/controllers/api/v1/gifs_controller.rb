5f2b03b9fa70b7b657a3a917a342ff7a272c3364
Leah Miller <lkmiller.87@gmail.com>
2019-02-25 13:25:09 -0700

    Adds change to method call within gifs controller

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::GifsController < ApplicationController
   def index
     gif = GifFacade.new(params[:location])
     # binding.pry
-    render json: GifSerializer.new(gif.get_gif_url)
+    render json: GifSerializer.new(gif.gif_daily_data)
   end
 end
