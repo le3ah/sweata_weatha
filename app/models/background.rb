59b603e76b89a71ca5a0ab68482085bf4151be97
Leah Miller <lkmiller.87@gmail.com>
2019-02-24 17:43:55 -0700

    Refactor of background model and removal of flickr model

--- app/models/background.rb
+++ app/models/background.rb
@@ -1,41 +1,15 @@
 class Background
-  attr_reader :city,
-              :state,
-              :latitude,
-              :longitude,
-              :background_img
-  def initialize(location)
-    city_state = location.split(',')
-    @city = city_state[0].capitalize
-    @state = city_state[1].upcase
-    @latitude = get_latitude
-    @longitude = get_longitude
-    @background_img = current_background
+  attr_reader :farm_id, :server_id, :id, :secret, :title
+  def initialize(attributes)
+    @farm_id = attributes[:farm]
+    @server_id = attributes[:server]
+    @id = attributes[:id]
+    @secret = attributes[:secret]
+    @title = attributes[:title]
+    @background_url = background_url
   end
 
-  def get_background_url
-    results = background_data[:photos][:photo][0]
-    Flickr.new(results)
+  def background_url
+    "https://farm#{@farm_id}.staticflickr.com/#{@server_id}/#{@id}_#{@secret}.jpg"
   end
-
-  def current_background
-    data = get_background_url
-    "https://farm#{data.farm_id}.staticflickr.com/#{data.server_id}/#{data.id}_#{data.secret}.jpg"
-  end
-  def get_latitude
-    location_data.find_latitude
-  end
-
-  def get_longitude
-    location_data.find_longitude
-  end
-
-  def location_data
-    @_location_data ||= GeocodingService.new(@city, @state)
-  end
-
-  def background_data
-    @_background_data ||= FlickrService.new.get_background(@latitude, @longitude)
-  end
-
 end
