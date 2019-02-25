9b0215f42e77b582cef45737046a2026769955b3
Leah Miller <lkmiller.87@gmail.com>
2019-02-24 20:21:59 -0700

    Adds missing attr readers

--- app/models/daily_weather.rb
+++ app/models/daily_weather.rb
@@ -1,9 +1,14 @@
 class DailyWeather
+  attr_reader :today_summary,
+              :precip_probability,
+              :precip_type,
+              :temperature_high,
+              :temperature_low
   def initialize(attributes)
-    @today_summary = attributes[:today_summary]
-    @precip_probability = attributes[:precip_probability]
-    @precip_type = attributes[:precip_type]
-    @temperature_high = attributes[:temperature_high]
-    @temperature_low = attributes[:temperature_low]
+    @today_summary = attributes[:summary]
+    @precip_probability = attributes[:precipProbability]
+    @precip_type = attributes[:precipType]
+    @temperature_high = attributes[:temperatureHigh]
+    @temperature_low = attributes[:temperatureLow]
   end
 end
