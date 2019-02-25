321940cf156e2d7159bf372d0a5c60976546911c
Leah Miller <lkmiller.87@gmail.com>
2019-02-24 20:13:45 -0700

    Updates model tests for forecast

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -1,25 +1,48 @@
-# require 'rails_helper'
-#
-# describe Forecast do
-#   it "exists", :vcr do
-#     location = "Denver,CO"
-#     forecast = Forecast.new(location)
-#     expect(forecast).to be_a(Forecast)
-#   end
-#   it "#get_latitude", :vcr do
-#     location = "Denver,CO"
-#     forecast = Forecast.new(location)
-#     expect(forecast.get_latitude).to eq(39.7392358)
-#   end
-#   it "#get_longitude", :vcr do
-#     location = "Denver,CO"
-#     forecast = Forecast.new(location)
-#     expect(forecast.get_longitude).to eq(-104.990251)
-#   end
-#   it "#get_tonight_summary", :vcr do
-#     location = "Denver,CO"
-#     forecast = Forecast.new(location)
-#     summary = forecast.weather_data[:hourly][:data].last[:summary]
-#     expect(forecast.get_tonight_summary).to eq(summary)
-#   end
-# end
+require 'rails_helper'
+
+describe Forecast do
+  before :each do
+    @attributes = {
+    "data": {
+        "id": "70278522252340",
+        "type": "forecast",
+        "attributes": {
+            "latitude": 39.7392358,
+            "longitude": -104.990251,
+            "currently": {
+                "icon": "clear-night",
+                "summary": "Clear",
+                "temperature_high": 45,
+                "temperature_low": 23,
+                "current_temperature": 33.37,
+                "current_time": " 7 pm",
+                "date": "2/24",
+                "feels_like": 30.63,
+                "humidity": 0.43,
+                "visability": 10,
+                "uv_index": 0
+            },
+            "daily": [
+                {
+                    "today_summary": "mostly cloudly",
+                    "precip_probability": 25,
+                    "precip_type": "snow",
+                    "temperature_high": 45,
+                    "temperature_low": 23
+                }, ],
+            "hourly": [
+                {
+                    "hourly_time": " 7",
+                    "hourly_temperature": 36.18,
+                    "hourly_icon": "clear-night"
+                },]
+        }
+    }
+}
+  end
+
+  it "exists", :vcr do
+    forecast = Forecast.new(@attributes)
+    expect(forecast).to be_a(Forecast)
+  end
+end
