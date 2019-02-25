daea5e0fa8c035d98614511f88f51e167065b88c
Leah Miller <lkmiller.87@gmail.com>
2019-02-24 17:44:33 -0700

    Updates expectations within background request spec

--- spec/requests/api/v1/background_request_spec.rb
+++ spec/requests/api/v1/background_request_spec.rb
@@ -3,18 +3,15 @@ require 'rails_helper'
 describe 'Background API' do
   it "retrieves an image based on the location", :vcr do
     location = create(:location)
-  
+
     get "/api/v1/backgrounds?location=#{location["city"]},#{location["state"]}"
 
     backgrounds = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
-    expect(backgrounds[:city]).to eq(location[:city])
-    expect(backgrounds[:state]).to eq(location[:state])
-    expect(backgrounds[:_background_data][:photos][:photo][0]).to have_key(:farm)
-    expect(backgrounds[:_background_data][:photos][:photo][0]).to have_key(:server)
-    expect(backgrounds[:_background_data][:photos][:photo][0]).to have_key(:id)
-    expect(backgrounds[:_background_data][:photos][:photo][0]).to have_key(:secret)
-    expect(backgrounds[:_background_data][:photos][:photo][0]).to have_key(:title)
+    expect(backgrounds[:data]).to have_key(:id)
+    expect(backgrounds[:data]).to have_key(:type)
+    expect(backgrounds[:data][:attributes]).to have_key(:title)
+    expect(backgrounds[:data][:attributes]).to have_key(:background_url)
   end
 end
