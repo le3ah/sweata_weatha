ab22d74aa87fbe521f08ea5386d0fe19967edeed
Leah Miller <lkmiller.87@gmail.com>
2019-02-25 09:53:26 -0700

    Adds correct api key for gif service call

--- app/services/gif_service.rb
+++ app/services/gif_service.rb
@@ -1,20 +1,18 @@
-# class GifService
-#
-#   def self.find_gifs(keyword)
-#     response = conn.get("/v1/gifs/search?") do |f|
-#       f.params[:q] = keyword
-#     end
-#     binding.pry
-#     JSON.parse(response.body, symbolize_names: true)
-#   end
-#
-#
-#
-#   def conn
-#     Faraday.new('https://api.giphy.com') do |f|
-#       f.params[:key] = ENV['flickr_api_key']
-#       f.params[:limit] = 5
-#       f.adapter Faraday.default_adapter
-#     end
-#   end
-# end
+class GifService
+
+  def find_gifs(keyword)
+    response = conn.get("/v1/gifs/search?") do |f|
+      f.params[:q] = keyword
+    end
+    binding.pry
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new('https://api.giphy.com') do |f|
+      f.params[:key] = ENV['GIPHY_API_KEY']
+      f.params[:limit] = 5
+      f.adapter Faraday.default_adapter
+    end
+  end
+end
