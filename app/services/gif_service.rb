6eb89e48341b104557e04e64bf19dc2b47f36267
Leah Miller <lkmiller.87@gmail.com>
2019-02-25 13:19:48 -0700

    Changes service methods to class methods

--- app/services/gif_service.rb
+++ app/services/gif_service.rb
@@ -1,6 +1,6 @@
 class GifService
 
-  def find_gifs(keyword)
+  def self.find_gifs(keyword)
     response = conn.get("/v1/gifs/search?") do |f|
       f.params[:q] = keyword
     end
@@ -9,7 +9,7 @@ class GifService
 
   private
 
-  def conn
+  def self.conn
     Faraday.new('https://api.giphy.com') do |f|
       f.params[:key] = ENV['GIPHY_API_KEY']
       f.params[:limit] = 5
