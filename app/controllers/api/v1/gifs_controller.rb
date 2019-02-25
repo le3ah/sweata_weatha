3ff3929d63d1a4446b8db81140247842b8b759c0
Leah Miller <lkmiller.87@gmail.com>
2019-02-25 09:53:54 -0700

    Updates gifs controller

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -1,19 +1,5 @@
 class Api::V1::GifsController < ApplicationController
   def index
-    def self.find_gifs(keyword)
-      conn = Faraday.new('https://api.giphy.com') do |f|
-        f.params[:key] = ENV['flickr_api_key']
-        f.params[:q] = keyword
-        f.params[:limit] = 5
-        f.adapter Faraday.default_adapter
-      end
-      response = conn.get("/v1/gifs/search?")
-      JSON.parse(response.body, symbolize_names: true)
-
-    end
-
-
-
-    # render json: Gif.new(params[:location])
+    render json: Gif.new(params[:location])
   end
 end
