9acac2b2988216e5dda712ff874e8d1e33c8221d
Leah Miller <lkmiller.87@gmail.com>
2019-02-25 09:10:11 -0700

    Adds test setup for gif request spec

---
+++ spec/requests/api/v1/gif_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'Gif API' do
+  it "provides animated gif based on the Dark Sky API" do
+    location = create(:location)
+
+    get "/api/v1/gifs?location=#{location["city"]},#{location["state"]}"
+
+    gifs = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(gifs[:data]).to have_key(:images)
+    expect(gifs[:data][:images][0]).to have_key(:time)
+    expect(gifs[:data][:images][0]).to have_key(:summary)
+    expect(gifs[:data][:images][0]).to have_key(:url)
+  end
+end
