65daab162a8b429dc447bd3896decf51ad193a22
Leah Miller <lkmiller.87@gmail.com>
2019-02-25 08:44:38 -0700

    Fixes spelling error

--- app/models/current_weather.rb
+++ app/models/current_weather.rb
@@ -8,19 +8,17 @@ class CurrentWeather
               :date,
               :feels_like,
               :humidity,
-              :visability,
+              :visibility,
               :uv_index
   def initialize(attributes)
     @icon = attributes[:icon]
     @summary = attributes[:summary]
-    @temperature_high = attributes[:temperatureHigh]
-    @temperature_low = attributes[:temperatureLow]
     @current_temperature = attributes[:temperature]
     @current_time = Time.at(attributes[:time]).strftime("%l %P")
     @date = Time.at(attributes[:time]).strftime("%-m/%-d")
     @feels_like = attributes[:apparentTemperature]
     @humidity = attributes[:humidity]
-    @visability = attributes[:visbility]
+    @visibility = attributes[:visibility]
     @uv_index = attributes[:uvIndex]
   end
 end
