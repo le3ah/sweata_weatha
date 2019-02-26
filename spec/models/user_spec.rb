1e8e095c23e6fa79221715843133e0881a8df3c1
Leah Miller <lkmiller.87@gmail.com>
2019-02-25 21:21:53 -0700

    Adds relationship test for users spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,8 @@ describe User, type: :model do
     it { should validate_presence_of(:email)}
     it { should validate_presence_of(:password)}
   end
+
+  describe 'relationships' do
+    it { should have_many :favorites }
+  end
 end
