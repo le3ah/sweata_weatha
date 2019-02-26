0f50b4be69990bd961d1743e5bcc0573c6c6078e
Leah Miller <lkmiller.87@gmail.com>
2019-02-25 19:09:46 -0700

    Adds test setup for sessions api

---
+++ spec/requests/api/v1/sessions_post_request_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'Sessions API' do
+  it "returns api key when email & password are accepted" do
+    User.create(email: "mom@gmail.com", password: "password", password_confirmation: "password")
+    email = "mom@gmail.com"
+    password = "password"
+
+    post "/api/v1/sessions?email=#{email}&password=#{password}"
+
+    expect(response).to be_successful
+
+    result = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(result).to have_key(:data)
+    expect(result[:data]).to have_key(:id)
+    expect(result[:data]).to have_key(:type)
+    expect(result[:data]).to have_key(:attributes)
+    expect(result[:data][:attributes]).to have_key(:api_key)
+  end
+end
