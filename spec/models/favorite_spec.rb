634e26fe0763e198d09e41a0268cd3938ae73839
Leah Miller <lkmiller.87@gmail.com>
2019-02-26 15:01:09 -0700

    Adds validations for favorites spec

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -3,4 +3,9 @@ describe Favorite, type: :model do
   describe 'relationships' do
     it { should belong_to :user }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :location}
+    it { should validate_presence_of :user_id}
+  end
 end
