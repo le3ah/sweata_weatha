28e1437d2279340120982bbd20024acfd4af3903
Leah Miller <lkmiller.87@gmail.com>
2019-02-25 18:02:50 -0700

    Adds updates to user create method within users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,16 @@
 class Api::V1::UsersController < ApplicationController
   def create
-binding.pry
+    @user = User.new(user_params)
+    if @user.save
+      @user.api_key = SecureRandom.hex(10)
+      @user.save
+      render json: UserSerializer.new(@user)
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
   end
 end
