2ad160601ef7ad333d45ed78f8f5501f77e349ff
Leah Miller <lkmiller.87@gmail.com>
2019-02-25 20:28:57 -0700

    Removes unnecessary lines

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::GifsController < ApplicationController
   def index
     gif = GifFacade.new(params[:location])
-    # binding.pry
     render json: GifSerializer.new(gif.gif_daily_data)
   end
 end
