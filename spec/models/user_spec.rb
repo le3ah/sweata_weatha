581a926f3492d48cc2df967518c61375f8828966
Leah Miller <lkmiller.87@gmail.com>
2019-02-25 18:00:38 -0700

    Adds validation test for user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:email)}
+    it { should validate_presence_of(:password)}
+  end
+end
