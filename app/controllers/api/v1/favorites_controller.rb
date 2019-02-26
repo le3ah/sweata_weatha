ee1ba2eabc50eed7005f73524c1a1b2a4813c23e
Leah Miller <lkmiller.87@gmail.com>
2019-02-26 15:50:20 -0700

    Adds destroy method favorites controller

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -16,8 +16,19 @@ class Api::V1::FavoritesController < ApplicationController
       else
         render json: "You have no favorite locations", status: :unauthorized
       end
-    else
-      render json: "You're not welcome here", status: :unauthorized
+    end
+  end
+
+  def destroy
+    if params[:api_key]
+      user = User.find_by(api_key: params[:api_key])
+      if user && user.favorites
+        favorite = user.favorites.find_by(location: params[:location])
+        favorite.destroy
+        render json: favorite
+      else
+        render json: "Something went wrong", status: :unauthorized
+      end
     end
   end
 end
