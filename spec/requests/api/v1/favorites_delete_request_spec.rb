4a7b8ac13c19bb464933a80154890affce472a16
Leah Miller <lkmiller.87@gmail.com>
2019-02-26 15:50:02 -0700

    Adds route for destroy favorites

--- spec/requests/api/v1/favorites_delete_request_spec.rb
+++ spec/requests/api/v1/favorites_delete_request_spec.rb
@@ -20,15 +20,33 @@ describe 'Favorites API - delete' do
     post "/api/v1/favorites?location=#{location_2[:city]},#{location_2[:state]}&api_key=#{api_key}"
     expect(response).to be_successful
 
-    get "/api/v1/favorites?api_key=#{api_key}"
+    delete "/api/v1/favorites?location=#{location_1[:city]},#{location_1[:state]}&api_key=#{api_key}"
     expect(response).to be_successful
 
-    delete "/api/v1/favorites?location=#{location_1[:city]},#{location_1[:state]}&api_key#{api_key}"
+    result = JSON.parse(response.body, symbolize_names: true)
+    expect(result[:location]).to eq("#{location_1[:city]},#{location_1[:state]}")
+  end
+  it "does not delete a favorite location without api key", :vcr do
+    email = "mom@gmail.com"
+    password = "123"
+    post "/api/v1/users?email=#{email}&password=#{password}&password_confirmation=#{password}"
     expect(response).to be_successful
 
-    deleted = JSON.parse(response.body, symbolize_names: true)
-    expect(deleted[:data][:attributes][:favorites][0][:location]).to eq("#{location_1[:city]},#{location_1[:state]}")
-    expect(deleted[:data][:attributes][:favorites][0][:current_weather]).to have_key(:temperature)
-    expect(deleted[:data][:attributes][:favorites][0][:current_weather]).to have_key(:summary)
+    result = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(result[:attributes]).to have_key(:api_key)
+    api_key = result[:attributes][:api_key]
+
+    location_1 = create(:location, city: "Cullman", state: "AL")
+    location_2 = create(:location, city: "Charleston", state: "SC")
+    post "/api/v1/favorites?location=#{location_1[:city]},#{location_1[:state]}&api_key=#{api_key}"
+    expect(response).to be_successful
+
+    post "/api/v1/favorites?location=#{location_2[:city]},#{location_2[:state]}&api_key=#{api_key}"
+    expect(response).to be_successful
+
+    delete "/api/v1/favorites?location=#{location_1[:city]},#{location_1[:state]}&api_key=123"
+    expect(response.body).to eq("Something went wrong")
+    expect(response.status).to eq(401)
   end
 end
