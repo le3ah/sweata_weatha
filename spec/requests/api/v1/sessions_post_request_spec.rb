9932938e98fdb7b371ea18215f3b248e5ed61e1c
Leah Miller <lkmiller.87@gmail.com>
2019-02-25 21:25:42 -0700

    Adds expectation for request test

--- spec/requests/api/v1/sessions_post_request_spec.rb
+++ spec/requests/api/v1/sessions_post_request_spec.rb
@@ -11,11 +11,20 @@ describe 'Sessions API' do
     expect(response).to be_successful
 
     result = JSON.parse(response.body, symbolize_names: true)
-    
+
     expect(result).to have_key(:data)
     expect(result[:data]).to have_key(:id)
     expect(result[:data]).to have_key(:type)
     expect(result[:data]).to have_key(:attributes)
     expect(result[:data][:attributes]).to have_key(:api_key)
+  end
+  it "returns error  when email & password are incorrect" do
+    User.create(email: "mom@gmail.com", password: "password", password_confirmation: "password")
+    email = "mom@gmail.com"
+    password = "password"
+
+    post "/api/v1/sessions?email=#{email}&password=123"
+
+    expect(response.body).to eq("This is bad")
   end
 end
