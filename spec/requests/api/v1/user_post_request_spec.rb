a6a21034f3d7f5762710e1cc827faf5417c6918e
Leah Miller <lkmiller.87@gmail.com>
2019-02-25 18:01:22 -0700

    Updates expectations in user post request spec

--- spec/requests/api/v1/user_post_request_spec.rb
+++ spec/requests/api/v1/user_post_request_spec.rb
@@ -4,10 +4,13 @@ describe 'Users API' do
   before { post '/api/v1/users', params: { "email" => "gob@bluth.com", "password" => "password", "password_confirmation" => "password"} }
 
   it "sends an email and password", :vcr do
-    post "/api/v1/users"
 
     expect(response).to be_successful
-    result = JSON.parse(response.body)
-    expect(result). to have_key("email")
+
+    result = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(result).to have_key(:id)
+    expect(result).to have_key(:type)
+    expect(result).to have_key(:attributes)
+    expect(result[:attributes]).to have_key(:api_key)
   end
 end
