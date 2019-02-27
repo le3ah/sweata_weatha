fb285ec978ff18b052056eafcb3c192fe31e288b
Leah Miller <lkmiller.87@gmail.com>
2019-02-26 18:20:15 -0700

    Adds attribute tests in weather poros

--- spec/models/current_weather_spec.rb
+++ spec/models/current_weather_spec.rb
@@ -12,7 +12,23 @@ describe CurrentWeather do
                   visbility: 45,
                   uvIndex: 45,
                   humidity: 23}
-    daily_weather = CurrentWeather.new(attributes)
-    expect(daily_weather).to be_a(CurrentWeather)
+    current_weather = CurrentWeather.new(attributes)
+    expect(current_weather).to be_a(CurrentWeather)
+  end
+  it "has attributes", :vcr do
+    attributes = {today_summary: "Sunny",
+                  icon: "Sunny",
+                  temperature: 83,
+                  time: 1551211200,
+                  apparentTemperature: 45,
+                  visbility: 45,
+                  uvIndex: 45,
+                  humidity: 23}
+    current_weather = CurrentWeather.new(attributes)
+    expect(current_weather.icon).to eq(attributes[:icon])
+    expect(current_weather.current_temperature).to eq(attributes[:temperature])
+    expect(current_weather.feels_like).to eq(attributes[:apparentTemperature])
+    expect(current_weather.uv_index).to eq(attributes[:uvIndex])
+    expect(current_weather.humidity).to eq(attributes[:humidity])
   end
 end
