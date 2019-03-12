c35b896db5a3043088171c1389c4003735488c6e
Leah Miller <lkmiller.87@gmail.com>
2019-03-12 14:28:53 -0600

    Comments out gif services info

--- app/facades/gif_facade.rb
+++ app/facades/gif_facade.rb
@@ -1,32 +1,32 @@
-class GifFacade
-  def initialize(location)
-    city_state = location.split(',')
-    @city = city_state[0].capitalize
-    @state = city_state[1].upcase
-  end
-
-  def location_data
-    @_location_data ||= GeocodingService.new.find_coordinates(@city, @state)
-  end
-
-  def forecast_data
-    lat = location_data[:lat]
-    long = location_data[:lng]
-    @_forecast_data ||= DarkSkyService.new.get_weather(lat, long)[:daily][:data]
-  end
-
-  def get_daily_weather
-    forecast_data.map do |raw_day|
-      DailyWeather.new(raw_day)
-    end
-  end
-
-  def gif_daily_data
-    get_daily_weather.map do |daily_weather|
-      url = GifService.find_gifs(daily_weather.precip_type)[0][:url]
-      time = daily_weather.time
-      summary = daily_weather.today_summary
-      Gif.new(url,time,summary)
-    end
-  end
-end
+# class GifFacade
+#   def initialize(location)
+#     city_state = location.split(',')
+#     @city = city_state[0].capitalize
+#     @state = city_state[1].upcase
+#   end
+#
+#   def location_data
+#     @_location_data ||= GeocodingService.new.find_coordinates(@city, @state)
+#   end
+#
+#   def forecast_data
+#     lat = location_data[:lat]
+#     long = location_data[:lng]
+#     @_forecast_data ||= DarkSkyService.new.get_weather(lat, long)[:daily][:data]
+#   end
+#
+#   def get_daily_weather
+#     forecast_data.map do |raw_day|
+#       DailyWeather.new(raw_day)
+#     end
+#   end
+#
+#   def gif_daily_data
+#     get_daily_weather.map do |daily_weather|
+#       url = GifService.find_gifs(daily_weather.precip_type).first[:url]
+#       time = daily_weather.time
+#       summary = daily_weather.today_summary
+#       Gif.new(url,time,summary)
+#     end
+#   end
+# end
--- spec/requests/api/v1/gif_request_spec.rb
+++ spec/requests/api/v1/gif_request_spec.rb
@@ -1,17 +1,17 @@
-require 'rails_helper'
-
-describe 'Gif API' do
-  it "provides animated gif based on the Dark Sky API", :vcr do
-    location = create(:location)
-
-    get "/api/v1/gifs?location=#{location["city"]},#{location["state"]}"
-
-    gifs = JSON.parse(response.body, symbolize_names: true)
-
-    expect(response).to be_successful
-    expect(gifs[:data][0]).to have_key(:attributes)
-    expect(gifs[:data][0][:attributes]).to have_key(:time)
-    expect(gifs[:data][0][:attributes]).to have_key(:summary)
-    expect(gifs[:data][0][:attributes]).to have_key(:url)
-  end
-end
+# require 'rails_helper'
+#
+# describe 'Gif API' do
+#   it "provides animated gif based on the Dark Sky API", :vcr do
+#     location = create(:location)
+#
+#     get "/api/v1/gifs?location=#{location["city"]},#{location["state"]}"
+#
+#     gifs = JSON.parse(response.body, symbolize_names: true)
+#
+#     expect(response).to be_successful
+#     expect(gifs[:data][0]).to have_key(:attributes)
+#     expect(gifs[:data][0][:attributes]).to have_key(:time)
+#     expect(gifs[:data][0][:attributes]).to have_key(:summary)
+#     expect(gifs[:data][0][:attributes]).to have_key(:url)
+#   end
+# end
