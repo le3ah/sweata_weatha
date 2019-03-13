2bb0dec75e55bf73c5b86c27d3128cf17868e097
Leah Miller <lkmiller.87@gmail.com>
2019-03-13 15:02:02 -0600

    Updates precip probability for front end rework

--- app/models/daily_weather.rb
+++ app/models/daily_weather.rb
@@ -8,7 +8,7 @@ class DailyWeather
   def initialize(attributes)
     # binding.pry
     @today_summary = attributes[:summary]
-    @precip_probability = attributes[:precipProbability]
+    @precip_probability = (attributes[:precipProbability].to_f * 100).round(0)
     @precip_type = attributes[:precipType]
     @temperature_high = attributes[:temperatureHigh]
     @temperature_low = attributes[:temperatureLow]
--- spec/models/daily_weather_spec.rb
+++ spec/models/daily_weather_spec.rb
@@ -20,7 +20,7 @@ describe DailyWeather do
                   time: 1551211200}
     daily_weather = DailyWeather.new(attributes)
     expect(daily_weather.today_summary).to eq(attributes[:summary])
-    expect(daily_weather.precip_probability).to eq(attributes[:precipProbability])
+    expect(daily_weather.precip_probability).to eq(8300)
     expect(daily_weather.precip_type).to eq(attributes[:precipType])
     expect(daily_weather.temperature_high).to eq(attributes[:temperatureHigh])
     expect(daily_weather.temperature_low).to eq(attributes[:temperatureLow])
